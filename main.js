f9f1d889042f61000c7e70c0a52159e1ec5c3712
Kiryll <robotsmarsa94@gmail.com>
2022-02-15 17:11:36 +0300

    add test remove func

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const closeModal = document.getElementById('close-modal')
 const appendPerson = document.getElementById('append-person')
 const overlay = document.getElementById('overlay')
 const btnDeleteLastUser = document.getElementById('delete-last-user')
-const btnDeleteUser = document.getElementById('delete-user')
-const removeUser = document.getElementById('remove')
 let userName = document.getElementById('name')
 let userLastname = document.getElementById('last-name')
 let userCountry = document.getElementById('country')
 let userCity = document.getElementById('city')
-
 // data
 const userArr = []
+
+
+
 
 
 // function open modal
@@ -53,14 +53,11 @@ appendPerson.addEventListener('click', () => {
    if (userName.value === '' || userLastname.value === '' || userCountry.value === '' || userCity.value === '') {
       alert('Заполните все поля')
    } else {
-      // userArr.push(new NewUser(userArr.length, userName.value, userLastname.value, userCountry.value, userCity.value))
       newUser(userArr.length, userName.value, userLastname.value, userCountry.value, userCity.value)
       drawTable()
       closeModalWindow()
-      findBtn()
-
+      console.log(userArr)
    }
-   console.log(userArr)
 
 })
 // delete last user
@@ -69,24 +66,6 @@ btnDeleteLastUser.addEventListener('click', () => {
    drawTable()
 })
 
-// function create user
-// let userInstance = null
-
-// function NewUser(id, name, lastName, country, city, removeButton) {
-//    if (userInstance) {
-//       return userInstance
-//    }
-//    instance = this
-//    this.id = id
-//    this.name = name
-//    this.lastName = lastName
-//    this.country = country
-//    this.city = city
-//    this.removeButton = `<button type="button" class="btn-close btn-remove" id="remove"></button>`
-
-//    return userInstance
-
-// }
 
 const newUser = (id, name, lastName, country, city, ) => {
    userArr.push({
@@ -95,17 +74,13 @@ const newUser = (id, name, lastName, country, city, ) => {
       lastName: lastName,
       country: country,
       city: city,
-      removeButton: `<button type="button" class="btn-close btn-remove" id="remove"></button>`
+      removeUser: `<button type="button" class="btn-close"></button>`,
    })
 }
 
 
-// draw table 
-// userArr.push(new NewUser(0, 'Bob', 'No-homo', 'Belarus', 'Minsk'))
-// userArr.push(new NewUser(1, 'Andrew', 'No-homo', 'Belarus', 'Minsk'))
-// userArr.push(new NewUser(2, 'Dick', 'No-homo', 'Belarus', 'Minsk'))
-// userArr.push(new NewUser(3, 'Jack', 'No-homo', 'Belarus', 'Minsk'))
 
+// draw table 
 const drawTable = () => {
    let tableHtml = ''
    userArr.forEach(elem => {
@@ -116,48 +91,29 @@ const drawTable = () => {
         <td class="table-warning">${elem.lastName}</td>
         <td class="table-warning">${elem.country}</td>
         <td class="table-warning">${elem.city}</td>
-        <td class="table-warning">${elem.removeButton}</td>
+        <td class="table-warning">${elem.removeUser}</td>
       </tr>`
    })
    document.getElementById('tbody').innerHTML = tableHtml
 }
 
-const findBtn = () => {
+
+
+const removeUserFunc = () => {
    let btnRemove = document.querySelectorAll('.btn-close')
-   for (let i = 0; i < userArr.length; i++) {
+   for (let i = 0; i < btnRemove.length; i++) {
       let deleteRow = btnRemove[i]
       deleteRow.addEventListener('click', () => {
-
-         console.log(userArr[i])
          let valueIndex = userArr.indexOf(userArr[i])
+         console.log(userArr[i])
+
          console.log(valueIndex)
-         console.log(userArr)
-         // if (i === valueIndex) {
-         //    userArr.splice(valueIndex)
-         //    console.log(userArr)
-         // }
+
+         userArr.splice(valueIndex, 1)
+
+         drawTable()
       })
 
    }
 }
-
-// const findKey = (obj) => {
-//    for (key in userArr) {
-//       if(key)
-//    }
-// }
-// findKey()
-
-
-
-// const removeUserFunc = () => {
-//    userArr.find((elem, index) => {
-//       if (index === findBtn()) {
-//          userArr.splice(removeBtn, 1)
-//          console.log(index)
-//          drawTable()
-//       }
-//    })
-// }
-// removeUserFunc()
-console.log(userArr)
+removeUserFunc()
