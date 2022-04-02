041062dbbf5a50d75f89bd3b80ee39dcef6213ae
Kiryll <robotsmarsa94@gmail.com>
2022-04-02 22:03:00 +0300

    test

--- main.js
+++ main.js
@@ -17,11 +17,6 @@ const userArr = []
 
 // function open modal
 const showModalWindow = () => {
-   userName.value = ''
-   userLastname.value = ''
-   userCountry.value = ''
-   userCity.value = ''
-
    modalWindow.classList.add('show')
    overlay.classList.add('overlay')
 }
@@ -34,6 +29,10 @@ const closeModalWindow = () => {
 // open modal
 newPersonBtn.addEventListener('click', () => {
    showModalWindow()
+   userName.value = ''
+   userLastname.value = ''
+   userCountry.value = ''
+   userCity.value = ''
 })
 
 // close modal
@@ -74,6 +73,12 @@ const newUser = (id, name, lastName, country, city, ) => {
       lastName: lastName,
       country: country,
       city: city,
+      editUser: `<a href="#" class="link-dark" onClick="editUser(${id})">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>
+      </a>`,
       removeUser: `<button type="button" class="btn-close" onClick="removeUser(${id})"></button>`,
    })
 }
@@ -86,12 +91,13 @@ const drawTable = () => {
    userArr.forEach(elem => {
       tableHtml += `
       <tr class="align-middle" id="row-users">
-        <td class="table-warning">${elem.id}</td>
-        <td class="table-warning">${elem.name}</td>
-        <td class="table-warning">${elem.lastName}</td>
-        <td class="table-warning">${elem.country}</td>
-        <td class="table-warning">${elem.city}</td>
-        <td class="table-warning">${elem.removeUser}</td>
+        <td class="table-warning td-user">${elem.id}</td>
+        <td class="table-warning td-user">${elem.name}</td>
+        <td class="table-warning td-user">${elem.lastName}</td>
+        <td class="table-warning td-user">${elem.country}</td>
+        <td class="table-warning td-user">${elem.city}</td>
+        <td class="table-warning td-user">${elem.editUser}</td>
+        <td class="table-warning td-user">${elem.removeUser}</td>
       </tr>`
    })
    document.getElementById('tbody').innerHTML = tableHtml
@@ -110,4 +116,9 @@ const removeUser = (id) => {
       }
 
    }
+}
+
+// edit user
+const editUser = (id) => {
+   showModalWindow()
 }
